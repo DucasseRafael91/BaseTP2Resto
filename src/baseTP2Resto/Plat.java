d75b11909d7c46716ce7a2eef00cbf48ae85d6ce
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-12-17 13:27:36 +0100

    Ajout du model plat avec les attributs

--- src/baseTP2Resto/Plat.java
+++ src/baseTP2Resto/Plat.java
@@ -1,5 +1,32 @@
 package baseTP2Resto;
 
 public class Plat {
+	
+	private int id;
+	private String nom;
+	
+    // Constructeur
+    public Plat(String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+    
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+    
+    //Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 
 }
