18011eb51350052fb450eac0f8700b5f232af45a
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-12-17 13:19:51 +0100

    Ajout de Dao

---
+++ src/baseTP2Resto/Dao.java
@@ -0,0 +1,17 @@
+package baseTP2Resto;
+
+import java.util.List;
+
+public interface Dao<T> {
+
+    void create(T obj);
+
+    void update(T obj);
+
+    void delete(int id);
+
+    T findById(int id);
+
+    List<T> findAll();
+}
+
